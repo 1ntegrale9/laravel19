1c29782eab9e2bfcc1994c426d2c42aedbeb5b69
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-20 17:47:32 +0900

    Implement: 投稿の削除

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::get('/', 'PostsController@index')->name('top');
-Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
 Route::resource('comments', 'CommentsController', ['only' => ['store']]);
