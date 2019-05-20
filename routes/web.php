f892d3fb0e1d3f51ed3643556cf78756a8601a77
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-20 17:38:18 +0900

    Implement: 投稿の編集

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::get('/', 'PostsController@index')->name('top');
-Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update']]);
 Route::resource('comments', 'CommentsController', ['only' => ['store']]);
