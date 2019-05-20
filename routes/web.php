70458c3864fe8a9a1c1920892bfbe63079107a35
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-20 17:25:01 +0900

    Implement: 投稿の詳細

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
 Route::get('/', 'PostsController@index')->name('top');
-Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
