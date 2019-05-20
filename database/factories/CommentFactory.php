6a793125356d2c7380d56589a3453a8839c77b58
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-20 15:22:42 +0900

    Fix: factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,12 +1,10 @@
+# database/migrations/UserFactory.php
 <?php
 
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
-
-use App\Comment;
 use Faker\Generator as Faker;
 
-$factory->define(Comment::class, function (Faker $faker) {
+$factory->define(App\Comment::class, function (Faker $faker) {
     return [
-        //
+        'body' => "コメントです。テキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。",
     ];
 });
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,12 +1,12 @@
+# database/migrations/PostFactory.php
+
 <?php
 
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
-
-use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => '投稿のタイトル',
+        'body' => "本文です。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。",
     ];
 });
