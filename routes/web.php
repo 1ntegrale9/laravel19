cd2270c8e2d3904e64dcadda362ceae8262d2335
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-21 19:47:40 +0900

    Fix: ログイン機能をBBSに統合

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+Auth::routes();
 Route::get('/', 'PostsController@index')->name('top');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
 Route::resource('comments', 'CommentsController', ['only' => ['store']]);
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
